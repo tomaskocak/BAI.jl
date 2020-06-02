b2385139b44b08ab1e1d4fcf0f0af74c93b6c414
Tomas Kocak <tomas.kocak@gmail.com>
2020-06-02 20:50:39 +0200

    Added reducers with tests

--- src/reducers.jl
+++ src/reducers.jl
@@ -6,14 +6,41 @@ function recover_index(a, j)
     return a < j ? a : a + 1
 end
 
+function reduce_omega(omega::Array{<:Real}, i::Int64, j::Int64)::Array{Float64}
+    omega_tilde = copy(omega)
+    omega_tilde[i] += omega_tilde[j]
+    omega_tilde = omega_tilde[[1:(j - 1); (j + 1):end]]
+    return copy(omega_tilde)
+end
 
-println(reduce_index(10, 3))
-println(reduce_index(10, 11))
-println(recover_index(10, 3))
-println(recover_index(10, 10))
-println(recover_index(10, 11))
+function reduce_lambda(lambda::Array{<:Real}, j::Int64)::Array{Float64}
+    new_lambda = copy(lambda)[[1:(j - 1); (j + 1):end]]
+    return copy(new_lambda)
+end
 
+function recover_lambda(lambda_tilde::Array{<:Real}, i::Int64, j::Int64, epsilon::Real)::Array{Float64}
+    lambda = [lambda_tilde[1:(j - 1)]; lambda_tilde[reduce_index(i, j)] + epsilon ; lambda_tilde[j:end]]
+    return copy(lambda)
+end
 
-function reduce_omega(omega, i, j)
-    # new_omega = zeros
+function reduce_mu(mu::Array{<:Real}, omega::Array{<:Real}, i::Int64, j::Int64, epsilon::Real)::Array{Float64}
+    mu_tilde = mu[[1:(j - 1); (j + 1):end]]
+    mu_tilde[reduce_index(i, j)] = (omega[i] * mu[i] + omega[j] * mu[j]) / (omega[i] + omega[j]) - (epsilon * omega[i]) / (omega[i] + omega[j])
+    return copy(mu_tilde)
+end
+
+function reduce_laplacian(Laplacian::Array{<:Real}, i::Int64, j::Int64)::Array{Float64}
+    Laplacian_tilde = copy(Laplacian)
+    Laplacian_tilde[i,:] += Laplacian_tilde[j,:]
+    Laplacian_tilde[:,i] += Laplacian_tilde[:,j]
+    Laplacian_tilde = Laplacian_tilde[[1:(j - 1); (j + 1):end], [1:(j - 1); (j + 1):end]]
+    # Laplacian_tilde[reduce_index(i, j), reduce_index(i, j)] -= sum(Laplacian_tilde[reduce_index(i, j), :])
+    return copy(Laplacian_tilde)
+end
+
+function reduce_laplacian_j(Laplacian::Array{<:Real}, i::Int64, j::Int64)::Array{Float64}
+    Laplacian_tilde_j = Laplacian[:, j]
+    Laplacian_tilde_j[j] += Laplacian_tilde_j[i]
+    Laplacian_tilde_j = Laplacian_tilde_j[[(1:i - 1); (i + 1):end]]
+    return copy(Laplacian_tilde_j)
 end
--- test/reducers_test.jl
+++ test/reducers_test.jl
@@ -1,8 +1,51 @@
 using BAI
 using Test
+using LinearAlgebra
+
+tolerance = 1e-14
 
 @test BAI.reduce_index(10, 3) == 9
 @test BAI.reduce_index(10, 11) == 10
 @test BAI.recover_index(10, 3) == 11
 @test BAI.recover_index(10, 10) == 11
 @test BAI.recover_index(10, 11) == 10
+
+
+# testing reduce_omega
+omega = [7, 13, 2, 7, 12]
+i, j = 3, 1
+omega_tilde = [13, 9, 7, 12]
+@test BAI.reduce_omega(omega, i, j) == omega_tilde
+
+# testing reduce_lambda
+lambda = [1.2, 3.5, 1.123, 7.32, 10.23, 1.5]
+j = 4
+lambda_tilde = [1.2, 3.5, 1.123, 10.23, 1.5]
+@test BAI.reduce_lambda(lambda, j) == lambda_tilde
+
+# testing recover_lambda
+lambda_tilde = [2.3, 7.41, 9.2, 10, 13.5]
+i, j = 4, 2
+epsilon = 0.01
+lambda = [2.3, 9.21, 7.41, 9.2, 10, 13.5]
+@test norm(BAI.recover_lambda(lambda_tilde, i, j, epsilon) - lambda) < tolerance
+
+# testing reduce_mu
+mu = [1.2, 3.4, 5.6, 7.8]
+omega = [0.1, 0.4, 0.3, 0.2]
+i, j = 2, 3
+epsilon = 0.01
+mu_tilde = [1.2, 4.337142857142857, 7.8]
+@test norm(BAI.reduce_mu(mu, omega, i, j, epsilon) - mu_tilde) < tolerance
+
+# testing reduce_laplacian
+laplacian = [2 -1 -1; -1 1.5 -0.5; -1 -0.5 1.5]
+i, j = 2, 1
+laplacian_tilde = [1.5 -1.5; -1.5 1.5]
+@test BAI.reduce_laplacian(laplacian, i, j) == laplacian_tilde
+
+# testing reduce_laplacian_j
+laplacian = [2 -1 -1; -1 1.5 -0.5; -1 -0.5 1.5]
+i, j = 2, 1
+laplacian_tilde_j = [1; -1]
+@test BAI.reduce_laplacian_j(laplacian, i, j) == laplacian_tilde_j
